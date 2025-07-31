f057e007201807d1f19771cf18b4e0fdb334e674
etzba <supoort@etzba.com>
2025-07-31 07:54:46 +0200

    use hash router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Route, HashRouter as Router, Switch} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router baseline='/'>
     <App />
-  </React.StrictMode>
+  </Router>
 );
